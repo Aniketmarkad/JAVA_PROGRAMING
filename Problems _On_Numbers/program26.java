a7abd66c72cf41a9bc4f8b3bd473423b5efe26c5
Aniket Markad <aniket45.markad@gmail.com>
2025-08-12 14:42:55 +0530

    Add files via upload

---
+++ Problems _On_Numbers/program26.java
@@ -0,0 +1,51 @@
+//check wheather no is divisible by 3 & 5 or only 3 or only 5
+
+import java.util.*;
+class Number
+{
+    void CheckDivisible(int ino)
+    {
+
+        if(((ino % 3) == 0 ) && ((ino % 5) == 0))
+        {
+            System.out.println(ino+" is divisible by both 3 & 5\n");
+        }
+        else if(((ino % 3)== 0))
+        {
+            System.out.println(ino+" is divible by 3\n");
+        }
+        else if((ino % 5) == 0)
+        {
+            System.out.println(ino+" is divisible by 5\n");
+        }
+
+        
+    }
+}
+
+public class program26
+{
+    public static void main(String a[])
+    {
+        Scanner sobj = new Scanner (System.in);
+        int ivalue = 0;
+       
+        System.out .println("enter the number :");
+        ivalue = sobj.nextInt();
+
+        Number nobj = new Number();
+
+        nobj.CheckDivisible(ivalue);
+
+    }
+        
+}
+
+
+
+
+
+
+
+
+
