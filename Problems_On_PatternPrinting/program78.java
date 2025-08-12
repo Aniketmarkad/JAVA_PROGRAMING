c478c76f218a9390671836af63bc4dcd51942eca
Aniket Markad <aniket45.markad@gmail.com>
2025-08-13 01:02:43 +0530

    Add files via upload

---
+++ Problems_On_PatternPrinting/program78.java
@@ -0,0 +1,59 @@
+// Input : 4    4
+/*
+  a a   a   a
+  B B   B   B
+  c c   c   c
+  D D   D   D
+
+*/
+
+import java.util.*;
+class Pattern
+{
+    void Display(int iRow,int iCol,char ch)
+    {
+        int j = 0 ,i = 0;
+        char ch1 = '\0';
+
+        for(i = 1,ch1 = 'a',ch ='A'; i <= iRow; i++,ch++,ch1++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i % 2 != 0)
+                {
+                    System.out.print(ch1+"\t");
+                }
+                else if( i% 2 == 0)
+                {
+                    System.out.print(ch+"\t");
+                }
+                
+                   
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+public class program78
+{
+    public static void main(String a[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("enter the no row :");
+        int iValue1 = sobj.nextInt();
+
+        System.out.println("enter the no of colmn :");
+        int iValue2 = sobj.nextInt();
+
+        System.out.println("enter the no row :");
+        char ch = sobj.next().charAt(0);
+        
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2,ch);
+        
+    }
+}
+
+
