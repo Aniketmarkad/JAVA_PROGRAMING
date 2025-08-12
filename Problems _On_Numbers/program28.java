f1c455843cad6e5b86960c77793f0db2f42edf40
Aniket Markad <aniket45.markad@gmail.com>
2025-08-12 14:40:33 +0530

    Add files via upload

---
+++ Problems _On_Numbers/program28.java
@@ -0,0 +1,57 @@
+// take input from user return its factorial also using final in it
+
+import java.util.*;
+class Number
+{
+    int Factorial(int iNo)
+    {
+        int icnt = 0;
+        int iFact = 1;
+
+        if( iNo < 0)
+        {
+            return program28.ERR_UNVALID;
+        }
+
+        for(icnt = 1; icnt <= iNo; icnt++) 
+        {
+            iFact = iFact * icnt;
+        }
+
+        return iFact;
+    
+    }
+}
+
+public class program28
+{
+    public static final int ERR_UNVALID = -1;
+    public static void main(String a[])
+    {
+        Scanner sobj = new Scanner (System.in);
+        int ivalue = 0,iRet = 0;
+
+        System.out .println("enter the number :");
+        ivalue = sobj.nextInt();
+
+        Number nobj = new Number();
+
+        iRet = nobj.Factorial(ivalue);
+
+        if(iRet == ERR_UNVALID)
+        {
+            System.out.println("Invalid  input unable to find factorial");
+        }
+        else
+        {
+            System.out.println("Factorial is :"+iRet);
+        }
+        
+    }
+}
+
+
+
+
+
+
