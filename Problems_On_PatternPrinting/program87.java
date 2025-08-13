82ad1211c2af57865ab1d95ba219c564bbdccddb
Aniket Markad <aniket45.markad@gmail.com>
2025-08-14 01:39:37 +0530

    Add files via upload

---
+++ Problems_On_PatternPrinting/program87.java
@@ -0,0 +1,47 @@
+// Input : 4    4
+/*
+    *
+    *   *
+    *   *   *
+    *   *   *   *
+*/
+
+import java.util.*;
+class Pattern
+{
+    void Display(int iRow,int iCol)
+    {
+        int j = 0 ,i = 0;
+
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= i; j++)
+            {
+                System.out.print("*\t");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+public class program87
+{
+    public static void main(String a[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("enter the no row :");
+        int iValue1 = sobj.nextInt();
+
+        System.out.println("enter the no of colmn :");
+        int iValue2 = sobj.nextInt();
+        
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+        
+    }
+}
+
+
+
